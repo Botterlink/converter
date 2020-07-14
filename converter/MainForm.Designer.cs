78ac27689e34c9a8697d9f38f574797aebcda850
Botterlink <botterlink@gmail.com>
2020-07-14 13:14:30 +0300

    first version

---
+++ converter/MainForm.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Converter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = " Открыть 7.1.0.6";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(230, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(276, 426);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(512, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(276, 426);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 55);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Сохранить в 7.1.0.7";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 136);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 37);
+            this.button3.TabIndex = 4;
+            this.button3.Text = " Открыть 7.1.0.7";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 179);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 35);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Сохранить в 7.1.0.6";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 245);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(211, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 271);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(211, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 297);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(212, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 323);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(212, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "MainForm";
+            this.Text = "Сonverter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ converter/MainForm.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using IniParser;
+using IniParser.Model;
+
+namespace Converter
+{
+    public partial class MainForm : Form
+    {
+        public string path;
+        public string path1;
+        public string path2;
+        public string fileName;
+        public string[] gk;
+        public string[] tk;
+        public string[] go;
+        public string[] to;
+        public FileIniDataParser parser;
+        public IniData data;
+        public FileIniDataParser parser1;
+        public IniData data1;
+        public FileIniDataParser parser2;
+        public IniData data2;
+
+        public MainForm()
+        {
+            InitializeComponent();
+#if !DEBUG
+            button1.Visible = false;
+            button2.Visible = false;
+#endif
+            textBox1.Text = @"term";
+            textBox2.Text = @"D:\коэфф. АЦП\";
+            textBox3.Text = @"D:\коэфф. АЦП\коэфф. АЦП 7.1.0.7\";
+            textBox4.Text = @"D:\коэфф. АЦП\Смещения нуля 7.1.0.7\";
+            //ParseFile();
+        }
+
+        private void ParseFile()
+        {
+            fileName = textBox1.Text;
+            path = textBox2.Text + fileName + ".txt";
+            path1 = textBox3.Text + fileName + ".txt";
+            path2 = textBox4.Text + fileName + ".txt";
+            parser = new FileIniDataParser();
+            if (!File.Exists(path))
+                using (File.Create(path)) { }
+            data = parser.ReadFile(path);
+            parser1 = new FileIniDataParser();
+            if (!File.Exists(path1))
+                using (File.Create(path1)) { }
+            data1 = parser.ReadFile(path1);
+            parser2 = new FileIniDataParser();
+            if (!File.Exists(path2))
+                using (File.Create(path2)) { }
+            data2 = parser.ReadFile(path2);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ParseFile();
+                richTextBox1.Clear();
+                richTextBox2.Clear();
+                int count = data["KoefChanelAdc"].Count;
+                gk = new string[count / 2];
+                tk = new string[count / 2];
+                go = new string[count / 2];
+                to = new string[count / 2];
+                foreach (SectionData section in data.Sections)
+                {
+                    int i = 0;
+                    foreach (KeyData key in section.Keys)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            if (section.SectionName == "KoefChanelAdc")
+                                gk[i / 2] = key.Value;
+                            else
+                                go[i / 2] = key.Value;
+                        }
+                        else
+                        {
+                            if (section.SectionName == "KoefChanelAdc")
+                                tk[(i - 1) / 2] = key.Value;
+                            else
+                                to[(i - 1) / 2] = key.Value;
+                        }
+                        i++;  
+                    }
+                }
+                for (int i = 0; i < count / 2; i++)
+                {
+                    richTextBox1.AppendText(gk[i] + System.Environment.NewLine);
+                    richTextBox1.AppendText(tk[i] + System.Environment.NewLine);
+                    richTextBox2.AppendText(go[i] + System.Environment.NewLine);
+                    richTextBox2.AppendText(to[i] + System.Environment.NewLine);
+                }
+                MessageBox.Show("считано");
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int count = gk[0].Split(';').GetLength(0);
+                for (int i = 0; i < gk.GetLength(0); i++)
+                {
+                    string[] gkSplit = gk[i].Split(';');
+                    string[] tkSplit = tk[i].Split(';');
+                    string[] goSplit = go[i].Split(';');
+                    string[] toSplit = to[i].Split(';');
+                    for (int j = 0; j < count; j++)
+                    {
+                        //data1.Sections.AddSection("E" + (j + 1).ToString());
+                        data1["E" + (j + 1).ToString()].AddKey((i + 1).ToString(), gkSplit[j].Trim().Replace(".", ",") + "; " + tkSplit[j].Trim().Replace(".", ",") + "; ; ; ; ; ; ; ; ; ; ; ");
+                        //data2.Sections.AddSection("E" + (j + 1).ToString());
+                        data2["E" + (j + 1).ToString()].AddKey((i + 1).ToString(), goSplit[j].Trim() + "; " + toSplit[j].Trim() + "; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0;");
+                    }
+                    parser1.WriteFile(path1, data1);
+                    parser2.WriteFile(path2, data2);
+                }
+                DelEmptyLines(path1);
+                DelEmptyLines(path2);
+                MessageBox.Show("сохранено");
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ParseFile();
+                richTextBox1.Clear();
+                richTextBox2.Clear();
+                if (data1.Sections.Count == 0)//если секций нет (файл создан в ТБ200) добавляем секцию [E1] в начало файла
+                {
+                    AddSection(path1);
+                    ParseFile();
+                }
+                string[] str = new string[data1.Sections.Count];
+                string[,] keyName = new string[data1.Sections.Count, 13];
+                int i = 0;
+                int j;
+                foreach (SectionData section in data1.Sections)
+                {
+                    str[i] = section.SectionName;
+
+                    j = 0;
+                    foreach (KeyData key in section.Keys)
+                    {
+                        int r = Convert.ToInt32(Regex.Replace(key.KeyName.Split('-')[0], @"[^\d]+", ""));//извлекаем число из имени ключа
+                        keyName[i, r - 1] = key.KeyName;
+                        j++;
+                    }
+                    i++;
+                }
+                for (i = 0; i < data1.Sections.Count; i++)
+                {
+                    for (j = 0; j < 13; j++)
+                    {
+                        if (keyName[i, j] == null) //если ключ отсутсвует то задаем стандартное название
+                            keyName[i, j] = (j + 1).ToString();
+                    }
+                }
+                gk = new string[13];
+                tk = new string[13];
+                go = new string[13];
+                to = new string[13];
+                for (i = 0; i < 13; i++)
+                {
+                    for (j = 0; j < data1.Sections.Count; j++)
+                    {
+                        string ch = String.Empty;
+                        if (j > 0) ch = ";";
+                        string values1;
+                        string values2;
+                        values1 = data1[str[j]][keyName[j, i]];
+                        values2 = data2[str[j]][keyName[j, i]];
+                        if (values1 == null)
+                            values1 = ";";
+                        if (values2 == null)
+                            values2 = ";";
+                        string[] values1Split = values1.Split(';');
+                        string[] values2Split = values2.Split(';');
+                        if (values1Split[0].Trim() == String.Empty) values1Split[0] = "1.0000";
+                        if (values1Split[1].Trim() == String.Empty) values1Split[1] = "1.0000";
+                        if (values2Split[0].Trim() == String.Empty) values2Split[0] = "6";
+                        if (values2Split[1].Trim() == String.Empty) values2Split[1] = "0";
+                        gk[i] += ch + values1Split[0].Trim().Replace(",", ".");
+                        tk[i] += ch + values1Split[1].Trim().Replace(",", ".");
+                        go[i] += ch + values2Split[0].Trim();
+                        to[i] += ch + values2Split[1].Trim();
+                    }
+                    richTextBox1.AppendText(gk[i] + System.Environment.NewLine + tk[i] + System.Environment.NewLine);
+                    richTextBox2.AppendText(go[i] + System.Environment.NewLine + to[i] + System.Environment.NewLine);
+                }
+                MessageBox.Show("считано");
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //data.Sections.AddSection("KoefChanelAdc");
+                //data.Sections.AddSection("OfsZeroChanelAdc");
+                for (int i = 0; i < gk.GetLength(0); i++)
+                {
+                    data["KoefChanelAdc"].AddKey((i * 2 + 1).ToString(), gk[i]);
+                    data["OfsZeroChanelAdc"].AddKey((i * 2 + 1).ToString(), go[i]);
+                    data["KoefChanelAdc"].AddKey((i * 2 + 2).ToString(), tk[i]);
+                    data["OfsZeroChanelAdc"].AddKey((i * 2 + 2).ToString(), to[i]);
+                }
+                parser.WriteFile(path, data);
+                DelEmptyLines(path);
+                MessageBox.Show("сохранено");
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void DelEmptyLines(string path)
+        {
+            var allLines = File.ReadAllLines(path, Encoding.UTF8).Where(s => !string.IsNullOrEmpty(s)).Distinct().ToArray();
+            File.WriteAllLines(path, allLines, Encoding.UTF8);
+        }
+
+        private void AddSection(string path)
+        {
+            var allLines = File.ReadAllLines(path, Encoding.UTF8).Where(s => !string.IsNullOrEmpty(s)).Distinct().ToArray();
+            File.WriteAllText(path, "[E1]" + System.Environment.NewLine, Encoding.UTF8);
+            File.AppendAllLines(path, allLines, Encoding.UTF8);
+        }
+    }
+}
